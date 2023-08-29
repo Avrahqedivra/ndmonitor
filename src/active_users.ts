a6aa67aa6f5a80192a7a65c38dbe1bacaf1d37ab
avrahqedivra <jmc_96@hotmail.com>
2023-08-30 00:30:44 +0200

    added comment, const, optimized

--- src/active_users.ts
+++ src/active_users.ts
@@ -33,57 +33,72 @@ const CURSOROFF = '\x1b[?25l'
 const ERASEEOL  = '\x1b[0K'
 const MARGIN    = 4
 
-console.log(`\n${BOLD}Active Users Utility v1.0 (c) 2023 Jean-Michel Cohen, F4JDN <f4jdn@outlook.fr>${ENDC}`)
-console.log(`\n${' '.repeat(MARGIN)}will create "${config.__path__}assets/active_users.json" using "${config.__log_path__}${config.__lastheard_file__}"`)
+const lasheardFile = `${config.__log_path__}${config.__lastheard_file__}`
+const subscribersFile = `${config.__path__}subscriber_ids.json`
+const localSubscribersFile = `${config.__path__}local_subscriber_ids.json`
+const activeUsersFile = `${config.__path__}assets/active_users.json`
 
-if (fs.existsSync(`${config.__log_path__}${config.__lastheard_file__}`)) {
-  if (fs.existsSync(`${config.__path__}assets/users.json`)) {
-    let users = JSON.parse(fs.readFileSync(`${config.__path__}assets/users.json`, 'utf-8'))['users']
+console.log(`\n${BOLD}Active Users Utility v1.1 (c) 2023 Jean-Michel Cohen, F4JDN <f4jdn@outlook.fr>${ENDC}`)
+console.log(`\n${' '.repeat(MARGIN)}will create "${activeUsersFile}" using "${lasheardFile}"`)
+
+if (fs.existsSync(`${lasheardFile}`)) {
+  if (fs.existsSync(`${subscribersFile}`)) {
+    let users = JSON.parse(fs.readFileSync(`${subscribersFile}`, 'utf-8'))['users']
     console.log(`\n${' '.repeat(MARGIN)}RadioId user file contains ${users.length} records`)
 
     // if available, add local subscribers
-    if (fs.existsSync(`${config.__path__}local_subscriber_ids.json`)) {
-      let local_subscribers = JSON.parse(fs.readFileSync(`${config.__path__}local_subscriber_ids.json`, 'utf-8'))['results']
+    if (fs.existsSync(`${localSubscribersFile}`)) {
+      let local_subscribers = JSON.parse(fs.readFileSync(`${localSubscribersFile}`, 'utf-8'))['results']
       console.log(`${' '.repeat(MARGIN)}local_subscribers_ids file contains ${local_subscribers.length} records`)
       users = users.concat(local_subscribers)
       console.log(`${' '.repeat(MARGIN)}After merge users dictionary contains ${users.length} records`)
     }
 
-    let traffic = JSON.parse(fs.readFileSync(`${config.__log_path__}${config.__lastheard_file__}`, 'utf-8'))['TRAFFIC']
+    let traffic = JSON.parse(fs.readFileSync(`${lasheardFile}`, 'utf-8'))['TRAFFIC']
 
-    let trafficLength = traffic.length
-    let singleUsers = new Set()
-    let record = null
-    let activeUsers: any[] = []
-    let maxprogbarlength = 40
-    let ratio = maxprogbarlength / trafficLength
-    let t1 = Date.now() / 1000 - 1
-    let elapsed: number = 0
-    let speed: number = 0
-    let eta: number = 0
-    let green: number = 0
-    let white: number = 0
+    let trafficLength       = traffic.length
+    let singleUsers         = new Set()
+    let record              = null
+    let activeUsers: any[]  = []
+    let maxprogbarlength    = 40
+    let ratio               = maxprogbarlength / trafficLength
+    let t1                  = Date.now()
+    let elapsed: number     = 0
+    let speed: number       = 0
+    let eta: number         = 0
+    let green: number       = 0
+    let white: number       = 0
   
     // prepare progression bar
     console.log(`${CURSOROFF}\r\nParsing the ${trafficLength} calls found in lastheard\n`)
 
+    // for each transmission in the lastheard file
     for(let i=0; i<trafficLength; i++) {
-      elapsed = (Date.now() / 1000) - t1
-      speed = Math.floor((i + 1) / elapsed)
-      eta = Math.floor((trafficLength-i) / speed)
-      green = Math.floor((i + 1) * ratio)
-      white = Math.floor(maxprogbarlength - green)
+
+      // prepapre data for the progression bar
+      elapsed = Date.now() - t1
+      speed   = Math.floor(i * 1000 / elapsed)
+      eta     = Math.floor((trafficLength-i) / speed)
+      green   = Math.floor(i * ratio)
+      white   = Math.floor(maxprogbarlength - green)
 
       // print progression bar
       process.stdout.write(`${' '.repeat(MARGIN)}${GREEN}${'━'.repeat(green)}${ENDC}${'━'.repeat(white)}${(i+1).toString().padStart(6)}/${trafficLength} ${RED}${speed} rec/s${ENDC} eta ${eta}s${ERASEEOL}\r`)
 
+      // read the record from lastheard traffic
       record = traffic[i]
 
+      // if the user dmrid is not already registered
       if (!singleUsers.has(record.DMRID)) {
+        // find his record in the radioid json file
         for(let j=0; j<users.length; j++) {
+          // if found in the radioid json file
           if (users[j].id == record.DMRID) {
+            // flag him as registered
             singleUsers.add(record.DMRID)
+            // add his rasioid record to the activeusers list
             activeUsers.push(users[j])
+            // stop there, go to the next
             break
           }
         }
@@ -92,7 +107,7 @@ if (fs.existsSync(`${config.__log_path__}${config.__lastheard_file__}`)) {
 
     console.log(`\n\nFound ${activeUsers.length} different OMs${CURSORON}\n`)
 
-    fs.unlinkSync(`${config.__path__}assets/active_users.json`)
-    fs.writeFileSync(`${config.__path__}assets/active_users.json`, JSON.stringify(activeUsers), "utf-8")
+    // write/overwrite the file
+    fs.writeFileSync(`${activeUsersFile}`, JSON.stringify(activeUsers), { encoding:'utf-8', flag:'w' })
   }
 }
