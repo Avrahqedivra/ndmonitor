a0389c3c460037693fbfa449c5d62a5dd895dc08
Jean-Michel Cohen <f4jdn@outlook.fr>
2024-03-22 12:13:58 +0100

    commends suitable for linux

--- src/extracommand.ts
+++ src/extracommand.ts
@@ -1,25 +1,30 @@
+import { exec } from "child_process";
+
 export const CMD_REBOOT:number          = 800		// REBOOT	
 export const CMD_SHUTDOWN:number       	= 801   // SHUTDOWN
 export const CMD_RESTART_MONITOR:number	= 802   // RESTART HBJSON
 export const CMD_RESTART_HBNET:number	  = 803   // RESTART HBNET	
 
+/**
+ * adapt commands to your needs
+ */
 export class Extra {
   rts_update(destination: number): void {
     switch(destination) {
       case CMD_REBOOT: 
-        // os.system('reboot')
+        exec('sudo /sbin/shutdown -r now', function (msg) { console.log(msg) });
         break
 
       case CMD_SHUTDOWN:
-        // os.system('shutdown')
+        exec('shutdown')
         break
 
       case CMD_RESTART_MONITOR:
-        // os.system('sudo systemctl restart hbjson.service')
+        exec('sudo systemctl restart ndmonitor.service')
         break
 
       case CMD_RESTART_HBNET:
-        // os.system('sudo systemctl restart hbnet.service')
+        exec('sudo systemctl restart hbnet.service')
         break
     }
   }
